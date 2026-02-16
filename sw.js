76325809ed4a38822835b62ed975f02b1e4d1527
mhmamadh7-collab <mhmamadh7@gmail.com>
2026-02-17 01:37:46 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const urlsToCache = [
    '/manifest.json',
    '/icon-192.png',
    '/icon-512.png',
-   '/TELLPhone1/sw.js' // این خط را اضافه کنید
+   '/TELLPhone/sw.js' // این خط را اضافه کنید
  ];
 
 self.addEventListener('install', event => {
@@ -24,4 +24,5 @@ self.addEventListener('fetch', event => {
         return response || fetch(event.request);
       })
   );
+
 });
